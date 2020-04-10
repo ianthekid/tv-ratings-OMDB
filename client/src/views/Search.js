15be6913e33453b9c8ed0e606f300c7158489b5c
Ian Ray <ianthekid@gmail.com>
2020-04-10 14:53:20 +0700

    Encode search params and show error for No Results

--- client/src/views/Search.js
+++ client/src/views/Search.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect, useRef, useCallback } from 'react';
 import { useHistory } from "react-router-dom";
-import { Row, Col, Form, Button } from 'react-bootstrap';
+import { Alert, Row, Col, Form, Button } from 'react-bootstrap';
 import { SearchResults } from './';
 
 function searchImdb(title, callback) {
-  fetch(`http://localhost:3001/s/${title}`)
+  fetch(`http://localhost:3001/s/${encodeURIComponent(title)}`)
   .then(res => res.json())
   .then(data => callback(data));
 }
@@ -14,6 +14,7 @@ function Search(props) {
   const history = useHistory();
   const [query, setQuery] = useState('');
   const [search, setSearch] = useState('');
+  const [error, setError] = useState(false);
   const [results, setResults] = useState([]);
   const [totalResults, setTotal] = useState(0);
   
@@ -25,8 +26,15 @@ function Search(props) {
     searchImdb(q, (res) => {
       setSearch('')
       setQuery(q)
-      setResults(res.Search)
-      setTotal(res.totalResults)
+    if(res.Response === "False") {
+        setError(true)
+        setTotal(0)
+        setResults([])
+      } else {
+        setError(false)
+        setResults(res.Search)
+        setTotal(res.totalResults)  
+      }
     })  
   }, []);
 
@@ -43,9 +51,6 @@ function Search(props) {
     <div>
       <Form onSubmit={handleSearch}>
         <Row>
-          <Col>Search results for "{query}"</Col>
-        </Row>
-        <Row>
           <Col xs={10}>
             <Form.Group controlId="showTitle">
               <Form.Control 
@@ -59,7 +64,15 @@ function Search(props) {
             <Button type="submit">Submit</Button>
           </Col>
         </Row>
+        <Row>
+          <Col className="text-left">Search results for "{query}"</Col>
+        </Row>
       </Form>
+      {(error) &&
+        <Alert variant="danger">
+          No Results Found
+        </Alert>
+      }
       {(totalResults > 0) &&
         <SearchResults 
           shows={results}
